69fbad32353ec073bf2a2cd9d82780630ef245b6
Christian Sletten <35894685+csletten@users.noreply.github.com>
2021-04-20 12:55:20 +0200

    Implemented the test set and found the error rate for the test set

--- Iris/iris.m
+++ Iris/iris.m
@@ -1,10 +1,14 @@
+clear W
+%% Constants
 alpha = 0.01;
-tol = 0.1;
+tol = 0.001;
 labels = ["Setosa" "Versicolor" "Virginica"];
 C = length(labels);
 N = 90;
+N_test = 60;
 D = 4;
 
+%% Training
 % Import data
 x = zeros(D, N);
 
@@ -18,17 +22,7 @@ data3 = load('class_3.txt', '-ascii');
 x(:, 61:90) = data3(1:30,:)'; 
 
 % Generate t matrix
-t = zeros(C, N);
-for i=1:N
-    if (i <= N/3)
-        t(:, i) = [1 0 0]';
-    elseif (i <= 2*N/3)
-        t(:, i) = [0 1 0]';
-    else 
-        t(:, i) = [0 0 1]';
-    end
-    
-end
+t1 = generate_solution(C, N);
 
 
 W0 = randn(C, D);
@@ -38,7 +32,7 @@ W = [W0 b0];
 sigm = @(z) 1./(1+exp(-z));
 
 while true
-    grad_MSE = gradient(sigm, x, W, t, N);
+    grad_MSE = gradient(sigm, x, W, t1, N);
     W = W - alpha*grad_MSE;
     
     if (norm(grad_MSE*alpha) < tol)
@@ -46,19 +40,57 @@ while true
     end
 end
 
-test_x = [6.1 2.9 4.7 1.4 1]';
-disp(sigm(W*test_x));
+%% Testing
+test = ones(D, N_test);
+test(:, 1:20) = data1(31:50,:)'; 
+test(:, 21:40) = data2(31:50,:)'; 
+test(:, 41:60) = data3(31:50,:)';
+
+test_results = ones(C, N_test);
+
+t2 = generate_solution(C, N_test);
+errors = 0;
+
+for i = 1:N_test
+    result = sigm(W*[test(:, i); 1]);
+    [~, class] = max(result);
+    binary_result = [0 0 0]';
+    binary_result(class) = 1;
+    test_results(:, i) = binary_result;
+    
+    if not(isequal(test_results(:, i), t2(:, i)))
+        disp("ERROR");
+        errors = errors + 1;
+    end
+end
+
+% Use plotConfusion() and histogram()
 
 function y = linearClassifier(sigm, x, W)
     y = sigm(W*[x' 1]');
 end
 
-function [grad_MSE, g] = gradient(sigm, x, W, t, N)
+function [grad_MSE, g] = gradient(sigm, x, W, t1, N)
     grad_MSE = 0;
     for k = 1:N
         g = linearClassifier(sigm, x(:, k), W);
-        grad_MSE = grad_MSE + ((g-t(:, k)).*g.*(1-g))*[x(:, k)' 1];
+        grad_MSE = grad_MSE + ((g-t1(:, k)).*g.*(1-g))*[x(:, k)' 1];
     end
 end
+
+function t = generate_solution(C, solution_length)
+    t = zeros(C, solution_length);
+    for i=1:solution_length
+        if (i <= solution_length/3)
+            t(:, i) = [1 0 0]';
+        elseif (i <= 2*solution_length/3)
+            t(:, i) = [0 1 0]';
+        else 
+            t(:, i) = [0 0 1]';
+        end
+    
+    end
+end
+
 
 

5be5d350347379fce0e6271b5a29ddf134b8bc52
Christian Sletten <35894685+csletten@users.noreply.github.com>
2021-04-21 13:16:46 +0200

    Implemented confusion matrices and simplified method to select training and test ranges

--- Iris/iris.m
+++ Iris/iris.m
@@ -1,25 +1,27 @@
-clear W
 %% Constants
 alpha = 0.01;
 tol = 0.001;
 labels = ["Setosa" "Versicolor" "Virginica"];
 C = length(labels);
-N = 90;
-N_test = 60;
+training_range = 1:30;
+test_range = 31:50;
+N = length(training_range)*3;
+N_test = length(test_range)*3;
 D = 4;
+
 
 %% Training
 % Import data
 x = zeros(D, N);
 
 data1 = load('class_1.txt', '-ascii');
-x(:, 1:30) = data1(1:30,:)'; 
+x(:, 1:30) = data1(training_range,:)'; 
 
 data2 = load('class_2.txt', '-ascii');
-x(:, 31:60) = data2(1:30,:)'; 
+x(:, 31:60) = data2(training_range,:)'; 
 
 data3 = load('class_3.txt', '-ascii');
-x(:, 61:90) = data3(1:30,:)'; 
+x(:, 61:90) = data3(training_range,:)'; 
 
 % Generate t matrix
 t1 = generate_solution(C, N);
@@ -40,16 +42,30 @@ while true
     end
 end
 
+training_results = ones(C, N);
+training_errors = 0;
+
+for i=1:N
+    result = sigm(W*[x(:, i); 1]);
+    [~, class] = max(result);
+    binary_result = [0 0 0]';
+    binary_result(class) = 1;
+    training_results(:, i) = binary_result;
+    if not(isequal(training_results(:, i), t1(:, i)))
+        training_errors = training_errors + 1;
+    end   
+end
+
 %% Testing
 test = ones(D, N_test);
-test(:, 1:20) = data1(31:50,:)'; 
-test(:, 21:40) = data2(31:50,:)'; 
-test(:, 41:60) = data3(31:50,:)';
+test(:, 1:20) = data1(test_range,:)'; 
+test(:, 21:40) = data2(test_range,:)'; 
+test(:, 41:60) = data3(test_range,:)';
 
 test_results = ones(C, N_test);
 
 t2 = generate_solution(C, N_test);
-errors = 0;
+test_errors = 0;
 
 for i = 1:N_test
     result = sigm(W*[test(:, i); 1]);
@@ -59,12 +75,16 @@ for i = 1:N_test
     test_results(:, i) = binary_result;
     
     if not(isequal(test_results(:, i), t2(:, i)))
-        disp("ERROR");
-        errors = errors + 1;
+        test_errors = test_errors + 1;
     end
 end
 
-% Use plotConfusion() and histogram()
+figure
+plotconfusion(training_results, t1);
+title("Confusion matrix for training set");
+figure
+plotconfusion(test_results, t2);
+title("Confusion matrix for test set");
 
 function y = linearClassifier(sigm, x, W)
     y = sigm(W*[x' 1]');

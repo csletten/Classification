de24f2490c3c7bd8570c72ec1c8dc29ed789e42a
Christian Sletten <35894685+csletten@users.noreply.github.com>
2021-04-22 14:44:29 +0200

    Added histograms and possibility to change classification feature set

--- Iris/iris.m
+++ Iris/iris.m
@@ -2,28 +2,34 @@ close all
 %% Constants
 alpha = 0.01;
 tol = 0.07;
-labels = ["Setosa" "Versicolor" "Virginica"];
-C = length(labels);
+
+classes = ["Setosa" "Versicolor" "Virginica"];
+C = length(classes);
+features = sort([1 2 3 4]);
+D = length(features);
+
 training_range = 1:30;
 test_range = 31:50;
 N_training = length(training_range)*3;
 N_test = length(test_range)*3;
-D = 4;
- 
- 
+
 %% Training
-% Import training data
-x = zeros(D, N_training);
- 
+training_data = zeros(D, N_training);
+
 data1 = load('class_1.txt', '-ascii');
-x(:, 1:30) = data1(training_range,:)';  
 data2 = load('class_2.txt', '-ascii');
-x(:, 31:60) = data2(training_range,:)'; 
 data3 = load('class_3.txt', '-ascii');
-x(:, 61:90) = data3(training_range,:)'; 
- 
-% Generate t matrix
-t1 = generate_solution(C, N_training);
+
+for i=1:length(features)
+    feature = features(i);
+    
+    training_data(i, 1:30) = data1(training_range,feature)';  
+    training_data(i, 31:60) = data2(training_range,feature)'; 
+    training_data(i, 61:90) = data3(training_range,feature)';
+    
+end
+
+training_solution = generate_solution(C, N_training);
  
 % Initialise W matrix 
 W0 = rand(C, D);
@@ -32,7 +38,7 @@ W = [W0 b0];
  
  
 while true
-   grad_MSE = gradient(x, W, t1, N_training, C, D);
+   grad_MSE = gradient(training_data, W, training_solution, N_training, C, D);
    W = W - alpha*grad_MSE;
     
    if (norm(grad_MSE) < tol) 
@@ -44,55 +50,74 @@ training_results = ones(C, N_training);
 training_errors = 0;
  
 for i=1:N_training
-    result = sigm(W*[x(:, i); 1]);
+    result = sigm(W*[training_data(:, i); 1]);
     [~, class] = max(result);
     binary_result = [0 0 0]';
     binary_result(class) = 1;
     training_results(:, i) = binary_result;
-    if not(isequal(training_results(:, i), t1(:, i)))
+    
+    if not(isequal(training_results(:, i), training_solution(:, i)))
         training_errors = training_errors + 1;
     end   
 end
  
 %% Testing
-% Import test data
-test = ones(D, N_test);
-test(:, 1:20) = data1(test_range,:)'; 
-test(:, 21:40) = data2(test_range,:)'; 
-test(:, 41:60) = data3(test_range,:)';
+test_data = ones(D, N_test);
+
+for i=1:length(features)
+    feature = features(i);
+    
+    test_data(i, 1:20) = data1(test_range,feature)'; 
+    test_data(i, 21:40) = data2(test_range,feature)'; 
+    test_data(i, 41:60) = data3(test_range,feature)';
+    
+end
  
+test_solution = generate_solution(C, N_test);
+
 test_results = ones(C, N_test);
- 
-t2 = generate_solution(C, N_test);
 test_errors = 0;
  
 for i = 1:N_test
-    result = sigm(W*[test(:, i); 1]);
+    result = sigm(W*[test_data(:, i); 1]);
     [~, class] = max(result);
     binary_result = [0 0 0]';
     binary_result(class) = 1;
     test_results(:, i) = binary_result;
     
-    if not(isequal(test_results(:, i), t2(:, i)))
+    if not(isequal(test_results(:, i), test_solution(:, i)))
         test_errors = test_errors + 1;
     end
 end
  
 figure
-plotconfusion(training_results, t1);
+plotconfusion(training_results, training_solution);
 title("Confusion matrix for training set");
 figure
-plotconfusion(test_results, t2);
+plotconfusion(test_results, test_solution);
 title("Confusion matrix for test set");
+
+
+for i=1:length(features)
+    feature = features(i);
+    figure
+    hold on
+    histogram(data1(:, feature));
+    histogram(data2(:, feature));
+    histogram(data3(:, feature));
+    hold off
+    title("Feature " + string(feature));
+    legend('Class 1', 'Class 2', 'Class 3');
+end
  
-function y = linearClassifier(x, W)
+function y = classification(x, W)
     y = sigm(W*[x' 1]');
 end
  
 function [grad_MSE, g] = gradient(x, W, t, N, C, D)
     grad_MSE = zeros(C, D + 1);
     for k = 1:N
-        g = linearClassifier(x(:, k), W);
+        g = classification(x(:, k), W);
         grad_MSE = grad_MSE + ((g-t(:, k)).*g.*(1-g))*[x(:, k)' 1];
     end
 end
